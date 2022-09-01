8a0aad140e5ce58ceba518202b2335ab476f6280
Gustavo De Souza <74631630+Gustavsouza@users.noreply.github.com>
2022-08-31 21:24:03 -0300

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// let reg = /^[\w.\+]+@\w+.\w{2,}(?:.\w{2})?$/;
+
+// if (reg.test(document.getElementById('Email'))) {
+//   alert("Email válido");
+// } else {
+//   alert("Email inválido");
+// }
