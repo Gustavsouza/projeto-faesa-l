28e43be8c4b53375cf9f0e6fe66d1021f336bc64
Gustavo De Souza <74631630+Gustavsouza@users.noreply.github.com>
2022-09-14 20:01:22 -0300

    Finish

--- javascript.js
+++
@@ -1,7 +0,0 @@
-// let reg = /^[\w.\+]+@\w+.\w{2,}(?:.\w{2})?$/;
-
-// if (reg.test(document.getElementById('Email'))) {
-//   alert("Email válido");
-// } else {
-//   alert("Email inválido");
-// }
